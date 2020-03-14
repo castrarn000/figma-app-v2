28e9f0203da0fe5bcf165e6e6bd515f292ec9541
Arnold Castro <arnold.castro@wsu.edu>
2020-03-14 09:38:20 -0700

    Restructure files and add init setup for new files

---
+++ src/UsersTable/UsersTable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavigationBar.sass';
+
+const UsersTable = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default UsersTable;
